e99b348f8349028699ea7a2a6867898be9892a31
gimo <goodmoskito@gmail.com>
2025-12-26 15:18:47 +0900

    feat: RSI indicator has been updated.

--- backend/domain/services.py
+++ backend/domain/services.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from dataclasses import dataclass
 
-from backend.domain.value_objects import SMAResult, EMAResult
+from backend.domain.value_objects import SMAResult, EMAResult, RSIResult
 
 
 class IndicatorService:
@@ -52,4 +52,43 @@ class IndicatorService:
             ema_20=float(ema_20),
             ema_60=float(ema_60),
             ema_120=float(ema_120)
+        )
+
+
+    @staticmethod
+    def get_relative_strength_index(prices: pd.Series) -> RSIResult:
+        """
+        Calculates RSI
+
+        Args:
+            prices: A pandas series of close prices.
+
+        Returns:
+            RSI Value Object
+        """
+
+        delta = prices.diff()
+        gain = delta.clip(lower=0)
+        loss = -1 * delta.clip(upper=0)
+
+        rsi_data = []
+        for period in (2, 7, 9, 14, 50):
+            avg_gain = gain.ewm(com=period - 1, adjust=False, min_periods=period).mean()
+            avg_loss = loss.ewm(com=period - 1, adjust=False, min_periods=period).mean()
+
+            rs = avg_gain / avg_loss
+            rsi_series = 100 - (100 / (1 + rs))
+
+            latest_rsi = rsi_series.iloc[-1]
+
+            if np.isnan(latest_rsi):
+                latest_rsi = 0.0
+            rsi_data.append(latest_rsi)
+
+        return RSIResult(
+            rsi_2=rsi_data[0],
+            rsi_7=rsi_data[1],
+            rsi_9=rsi_data[2],
+            rsi_14=rsi_data[3],
+            rsi_50=rsi_data[4],
         )
