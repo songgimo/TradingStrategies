4f5e37f47901a4a5e96775e041c75dad12f66c83
gimo <goodmoskito@gmail.com>
2025-12-26 17:12:07 +0900

    feat: Remove unused functions in output.py

--- backend/application/ports/output.py
+++ backend/application/ports/output.py
@@ -1,32 +1,14 @@
 from abc import ABC, abstractmethod
-from backend.domain.value_objects import Ticker, Stock, DataOHLCV
+from backend.domain.value_objects import Ticker, Stock, DataOHLCV, Symbol
 from backend.domain.reference_data import StockMarketType, CryptoMarketType
 import typing
 import pandas as pd # Pragmatic exception!
+from datetime import date
 
 
 class StockMarketOutputPort(ABC):
-    def get_ohlcv_history(self, symbol: str) -> pd.DataFrame:
-        ...
-
     @abstractmethod
-    def get_orderbook(self, symbol: str) -> pd.DataFrame:
-        ...
-
-    @abstractmethod
-    def get_dividend_history(self, stock: Stock) -> typing.List[Stock]:
-        ...
-
-    @abstractmethod
-    def get_stock_reports(self, stock: Stock) -> typing.List[Stock]:
-        ...
-
-    @abstractmethod
-    def get_stock_ohlc(self, stock: Stock) -> typing.List[Stock]:
-        ...
-
-    @abstractmethod
-    def get_all_stocks(self, market: StockMarketType) -> typing.List[Stock]:
+    def get_candle_history(self, target: Symbol, start: date, end: date) -> pd.DataFrame:
         ...
 
 
@@ -36,12 +18,5 @@ class CryptoMarketOutputPort(ABC):
     SECRET: str
 
     @abstractmethod
-    def get_ticker(self, symbol: str) -> Ticker:
-        ...
-
-    def get_ohlcv_history(self, symbol: str) -> pd.DataFrame:
-        ...
-
-    @abstractmethod
-    def get_orderbook(self, symbol: str) -> pd.DataFrame:
+    def get_candle_history(self, symbol: Symbol) -> pd.DataFrame:
         ...
