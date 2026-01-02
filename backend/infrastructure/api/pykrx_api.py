63181e42fcea2bfacc876b447102e629fe37d195
gimo <goodmoskito@gmail.com>
2026-01-02 16:34:48 +0900

    feat: pykrx_api fixed

--- backend/infrastructure/api/pykrx_api.py
+++ backend/infrastructure/api/pykrx_api.py
@@ -12,8 +12,8 @@ logger = logging.getLogger(__name__)
 
 class PykrxAPI(MarketOutputPort):
     _ENG_COLUMNS = {
-        '시가': 'Open', '고가': 'High', '저가': 'Low',
-        '종가': 'Close', '거래량': 'Volume', '등락률': 'Change'
+        '시가': 'open_price', '고가': 'high_price', '저가': 'low_price',
+        '종가': 'close_price', '거래량': 'volume'
     }
     _INTERVAL_MAP = {
         Interval.DAY: 'd',
@@ -21,22 +21,12 @@ class PykrxAPI(MarketOutputPort):
         Interval.MONTH: 'm'
     }
 
-    def _validate_and_get_freq(self, interval: Interval) -> str:
-        frq = self._INTERVAL_MAP.get(interval)
-        if not frq:
-            logger.error(f"Invalid interval '{interval}'")
-            raise ValueError("Pykrx only supports Day, Week, and Month intervals.")
-        return frq
-
-    def _post_process_df(self, df: pd.DataFrame, count: int) -> pd.DataFrame:
-        df.index.name = 'Date'
-        df = df.sort_index(ascending=False).tail(count)
-        df.rename(columns=self._ENG_COLUMNS, inplace=True)
-        return df
-
     def get_candle_history(self, target: Symbol, interval: Interval, count: int = 1) -> pd.DataFrame:
         logger.info(f"Fetching candle history: ticker={target}, interval={interval}, count={count}")
-        freq = self._validate_and_get_freq(interval)
+        freq = self._INTERVAL_MAP.get(interval)
+        if not freq:
+            logger.error(f"Invalid interval '{interval}'")
+            raise ValueError("Pykrx only supports Day, Week, and Month intervals.")
 
         ohlcv_df = pd.DataFrame()
         to_date = datetime.datetime.now()
@@ -52,15 +42,38 @@ class PykrxAPI(MarketOutputPort):
                 break
             to_date = from_date
 
-        return self._post_process_df(ohlcv_df, count)
+        ohlcv_df.index.name = 'candle_date_time'
+        df = ohlcv_df.sort_index(ascending=False).tail(count)
+        df.rename(columns=self._ENG_COLUMNS, inplace=True)
+        return df
 
-    def get_candles_history(self, targets: List[Symbol], interval: Interval, count: int = 200) -> pd.DataFrame:
-        logger.info(f"Fetching candle history: ticker={targets}, interval={interval}, count={count}")
-        freq = self._validate_and_get_freq(interval)
-
+    def get_candles_last_day_history(self, targets: List[Symbol], market: StockMarketType) -> pd.DataFrame:
+        logger.info(f"Fetching candle history: ticker={targets}")
         to_date = datetime.datetime.now()
-        from_date = to_date - datetime.timedelta(days=count)
-        from_str, to_str = from_date.strftime("%Y%m%d"), to_date.strftime("%Y%m%d")
-        ohlcv_df = stock.get_market_ohlcv(from_date=from_str, to_date=to_str, freq=freq)
+        ohlcv_df = pd.DataFrame()
 
-        return self._post_process_df(ohlcv_df, count)
+        for _ in range(10):
+            from_date = to_date - datetime.timedelta(days=1)
+            from_str, to_str = from_date.strftime("%Y%m%d"), to_date.strftime("%Y%m%d")
+            data = stock.get_market_ohlcv(date=from_str, market=str(market))
+            if data.iloc[-1].values[-1] != 0:
+                ohlcv_df = data
+                break
+
+            to_date = from_date
+        ohlcv_df.drop(columns=["거래대금", "등락률", "시가총액"], inplace=True)
+        ohlcv_df.index.name = 'stock_code'
+        ohlcv_df.rename(columns=self._ENG_COLUMNS, inplace=True)
+        return ohlcv_df
+
+    def get_all_symbols(self, market_type: StockMarketType):
+        ...
+        # logger.info("Fetching all symbols from markets.")
+        #
+        # if not market_type in [market_type.KOSPI, market_type.KOSDAQ]:
+        #     logger.error(f"Invalid MarketType '{market_type}'")
+        #     raise ValueError("Pykrx only supports KOSPI and KOSDAQ markets")
+        #
+        # json_file = settings.json_file_path + "stock_codes.json"
+        # with open(json_file, "r") as f:
+        #     data = json.loads(f.read())
