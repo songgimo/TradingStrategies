ee6e43e59b815fffa0da85020cc62a81f8eac3b8
gimo <goodmoskito@gmail.com>
2026-01-02 16:33:38 +0900

    feat: app_services & input port fixed.

--- backend/application/app_services.py
+++ backend/application/app_services.py
@@ -1,39 +1,45 @@
 import logging
 from typing import List
 
-from backend.application.ports.input import CollectMarketDataUseCase
-from backend.application.ports.output import MarketOutputPort
+import pandas as pd
+
+from backend.application.ports.output import MarketOutputPort, DatabaseOutputPort
 from backend.domain.value_objects import Symbol
 from backend.domain.reference_data import Interval, StockMarketType
 
 logger = logging.getLogger(__name__)
 
 
-class MarketDataService(CollectMarketDataUseCase):
+class CollectMarketDataService:
     """
-    Application service implementing CollectMarketDataUseCase
+        Use case for collecting market data.
 
-    Orchestrates data collection from market APIs and storage
+        Orchestrates:
+        - Fetching OHLCV data from APIs
+        - Storing data in database.
+        - Error handling and retry logic
     """
 
     def __init__(
             self,
-            market_output_port: MarketOutputPort,
+            market_port: MarketOutputPort,
+            database_port: DatabaseOutputPort,
+            market: StockMarketType
     ):
-        self._market_output_port = market_output_port
+        self._market_output_port = market_port
+        self._database_port = database_port
+        self._market = market
 
-    def collecting_ohlcv(
-            self,
-            targets: List[Symbol],
-            interval: Interval,
-            count: int
-    ):
-        self._market_output_port.get_candles_history(
-            targets,
-            interval,
-            count
-        )
-        ...
+    def execute(self):
+        if self._market in [StockMarketType.KOSPI, StockMarketType.KOSDAQ]:
+            history_data = self._market_output_port.get_candles_last_day_history(
+                [],
+                self._market
+            )
 
-    def get_all_symbols(self, market: StockMarketType) -> List[Symbol]:
-        ...
+        else:
+            raise ValueError(
+                f"Unsupported market type: {self._market}"
+            )
+
+        self._database_port.put_ohlcv_to_database(history_data)
